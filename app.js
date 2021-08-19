6d15fa06fea5e19a87ce17a1f998cc40ef136830
Atryom <238136@mail.ru>
2021-08-19 13:32:35 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const path = require('path');
+const todoRoutes = require('./routes/todos');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+});
+
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(todoRoutes);
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('mongo'), {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}...`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
